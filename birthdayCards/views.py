394d2da7b3b20deb502915e19178bb713fef67c2
Rakesh-Racky <rakeshyadhavan03@gmail.com>
2026-02-22 20:09:13 +0530

    card start

---
+++ birthdayCards/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+def design(request):
+    return render(request, "design.html")
